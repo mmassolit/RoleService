473c01a76f9d075a7009aa97c2128bf06fe92c00
Maxim <mmassolit@gmail.com>
2020-11-24 09:47:00 +0200

    Bugs fixed

--- roleService/src/main/java/com/videosharing/service/IRoleService.java
+++ roleService/src/main/java/com/videosharing/service/IRoleService.java
@@ -2,7 +2,6 @@ package com.videosharing.service;
 
 import java.util.List;
 
-import org.springframework.data.domain.Page;
 
 import com.videosharing.model.Role;
 
@@ -12,8 +11,6 @@ import javassist.NotFoundException;
 public interface IRoleService {	
 	List<Role> findAll();
 
-	Page<Role> findPaginated(int page, int size);
-	
     Role save(Role adForSave);
     
     Role getById(String adId) throws NotFoundException;
